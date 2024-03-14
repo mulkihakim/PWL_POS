f41d3eeed676f01a4493181f34f863a964df4116
mulkihakim <mulkihkm@gmail.com>
2024-03-14 13:45:17 +0700

    pwl js 3 praktikum 4

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\HomeController;
+use App\Http\Controllers\LevelController;
 use App\Http\Controllers\PenjualanController;
 use App\Http\Controllers\ProductsController;
 use App\Http\Controllers\UserController;
@@ -20,6 +21,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('/level', [LevelController::class, 'index']);
 Route::get('/', [HomeController::class, 'index']);
 Route::get('/user/{id}/{name}', [UserController::class, 'profile']);
 Route::prefix('category')->group(function() {
