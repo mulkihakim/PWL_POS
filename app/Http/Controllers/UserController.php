bda25a3c5725f5f2c84724e5eed50100acde9d80
mulkihakim <mulkihkm@gmail.com>
2024-04-15 00:36:53 +0700

    pwl js 4 praktikum 2.1

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -9,19 +9,28 @@ use Illuminate\Support\Facades\Hash;
 class UserController extends Controller
 {
     public function index() {
-        // tambah data user dengan Eloquent Model
-        $data = [
-            'level_id' => 2, 
-            'username' => 'manager_tiga',
-            'nama' => 'Manager 3', 
-            'password' => Hash::make('12345')
-        ];
-        UserModel::create($data);
+        // // tambah data user dengan Eloquent Model
+        // $data = [
+        //     'level_id' => 2, 
+        //     'username' => 'manager_tiga',
+        //     'nama' => 'Manager 3', 
+        //     'password' => Hash::make('12345')
+        // ];
+        // UserModel::create($data);
+
         // coba akses model UserModel
-        $user = UserModel::all(); // ambil semua data dari tabel m_user
+        // $user = UserModel::all(); // ambil semua data dari tabel m_user
+
+        // RETRIEVING SINGLE MODELS
+        // $user = UserModel::find(1); // ambil satu data dari tabel m_user
+        // $user = UserModel::where('level_id', 1)->first(); // ambil satu data dari tabel m_user
+        // $user = UserModel::firstWhere('level_id', 1); // ambil satu data dari tabel m_user
+        $user = UserModel::findOr(20, ['username', 'nama'], function () {
+            abort(404);
+        }); // ambil satu data dari tabel m_user
         return view('user', ['data' => $user]);
         
-        // // UserModel::insert($data); // tambahkan data ke tabel m_user
+        // UserModel::insert($data); // tambahkan data ke tabel m_user
         // UserModel::where('username', 'customer-1')->update($data); // update data user
 
     }
