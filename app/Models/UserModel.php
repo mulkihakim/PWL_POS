d87bdabac24c81a9dab1ab4b5e2daed0e453c816
mulkihakim <mulkihkm@gmail.com>
2024-04-14 23:31:37 +0700

    pwl js 4 praktikum 1

--- app/Models/UserModel.php
+++ app/Models/UserModel.php
@@ -9,6 +9,13 @@ class UserModel extends Model
 {
     use HasFactory;
 
-    protected $table = 'm_user';        //Mendefinisikan nama tabel yang digunakan ileh model ini
+    protected $table = 'm_user';        //Mendefinisikan nama tabel yang digunakan oleh model ini
     protected $primaryKey = 'user_id';  //Mendefinisikan primary key dari tabel yang digunakan
+
+    protected $fillable = [
+        'level_id',
+        'username',
+        'nama',
+        // 'password'
+    ];
 }
